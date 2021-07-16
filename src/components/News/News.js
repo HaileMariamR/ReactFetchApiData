9035e1bde46a861ea7ac3b670701376bf0d2718d
Hailemariam Fikadie <se.hailemariam.fikadie@gmail.com>
2021-07-16 16:20:12 +0300

    update the functionality with react hooks

--- src/components/News/News.js
+++ src/components/News/News.js
@@ -1,4 +1,4 @@
-import {React  ,useState} from 'react'
+import {React  ,useState, useEffect} from 'react'
 import { ReactDOM  } from 'react'
 import {SingleNews} from './SingleNews'
 import axios from 'axios'
@@ -7,7 +7,7 @@ import axios from 'axios'
 
 export const News = ()=> {
 
-    const [news , setNewsValue] = useState(["haile"]);
+    const [news , setNewsValue] = useState([]);
 
    const  renderItems =()=>{
         return news.map((eachnews , index)=>
@@ -25,9 +25,11 @@ export const News = ()=> {
          })
      }
 
+
      const getnewsbyaxiosmethod =()=>{
         const url = 'https://newsapi.org/v2/everything?q=tesla&from=2021-07-13&to=2021-07-13&sortBy=popularity&apiKey=28b97d72f5144c86ba7d64a9e9892654';
-        axios.get(url)
+        axios
+            .get(url)
             .then((response)=>{
                 setNewsValue(response.data.articles);
                console.log(response.data.articles);
@@ -35,7 +37,17 @@ export const News = ()=> {
             });
 
      }
-     
+
+     useEffect(()=>{
+        const url = 'https://newsapi.org/v2/everything?q=cnn&from=2021-07-13&to=2021-07-13&sortBy=popularity&apiKey=28b97d72f5144c86ba7d64a9e9892654';
+        axios
+            .get(url)
+            .then((response)=>{
+                setNewsValue(response.data.articles);
+               console.log(response.data.articles);
+
+            });
+     })
 
         
     return (
@@ -43,17 +55,23 @@ export const News = ()=> {
          
         <nav>
             <div class="nav-wrapper">
-            <a href="#" class="brand-logo">My Feeds.....</a>
+            <a href="#" class="brand-logo">My Feeds</a>
+
+            
+
+
           
             </div>
         </nav>
 
+       
+
 
             <div className='row'>
-                 <ul> {renderItems()}</ul>
+                 <ul> {(news.length == 0)?<h1> "Loading News..." </h1> :renderItems()}</ul>
             </div>
             
-            <button onClick={()=>getnewsbyaxiosmethod()}>seeNews</button>
+            {/* <button onClick={()=>getnewsbyaxiosmethod()}>seeNews</button> */}
         </div>
     )
     
